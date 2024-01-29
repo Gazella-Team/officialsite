e53cdfe5a8def98bc754c7a917496ad9586f4ee9
Lasse <lass.osmann@gmail.com>
2024-01-29 17:53:42 +0100

    bæo

--- src/pages/udvikling/arbejdsprocesser.tsx
+++ src/pages/udvikling/arbejdsprocesser.tsx
@@ -13,7 +13,6 @@ export default function Arbejdsprocesser() {
         icon={<ArrowLeftRight color="#ffffff" size={70} />}
       />
       <Description description="Få et indblik i, hvordan vi eksekverer udviklingen af din kommende web-løsning fra start til slut. For os er det altafgørende at have en struktureret process, som gør projektet så overskueligt og effektivt som muligt for begge parter. De bedste løsninger skabes af god planlægning." />
-      <Processes />
     </Layout>
   );
 }
