7c75a5a443a920aa8d127e29b757800b059550c4
Lasse Osmann <lasseosmann@Lasses-MacBook-Pro.local>
2024-02-05 09:17:02 +0100

    animate

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -25,7 +25,7 @@ export default function Layout({
           <Meta />
 
           <Navbar />
-          <div className="pt-[90px] members:pt-[90px]">
+          <div className="pt-[90px]">
             {children}
 
             <Footer />
