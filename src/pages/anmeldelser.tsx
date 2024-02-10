3ff2893c0bd7434df1bac38598bd36c3eb328503
Lasse Osmann <lasseosmann@Lasses-MBP.home>
2024-02-10 13:50:38 +0100

    o+

--- src/pages/anmeldelser.tsx
+++ src/pages/anmeldelser.tsx
@@ -14,6 +14,11 @@ export default function Anmeldelser() {
         description="Vi arbejder hele tiden på at skrive nye artikler, hvor vores team deler ud af tips & tricks i forhold til hvordan du laver din hjemmeside."
       />
       <Hero
+                  description={<div className="w-[92%] mx-auto max-w-main flex items-center justify-end">
+                  <p className="text-[22px] w-[68%] text-gray-600 font-[400] lg:text-[24px] lg:leading-[34px] lg:w-[90%] mid:w-full mid:text-[22px] mid:leading-[32px]">
+                  Gazella Team udgør et hold af ambitiøse, nytænkende og storsmilende programmøre og designere, som hver dag brænder for at skabe unikke digitale produkter. Vi fokuserer på, at tilpasse hver enkel løsning til det pågældende projekt. Derfor kan du hos os forvente en fast kompetent sparingspartner, som supplerer med viden og leverer til tiden til den aftalte pris.
+                  </p>
+                </div>}
       background="bg-[linear-gradient(rgba(5,29,64,0.30),rgba(5,29,64,1)),url(/herobg/webdev.jpg)] bg-top bg-cover"
         heading="Anmeldelser"
         section="kunder"
--- src/pages/arbejde/cases/coad.tsx
+++ src/pages/arbejde/cases/coad.tsx
@@ -8,8 +8,11 @@ import Link from "next/link";
 export default function Coad() {
   return (
     <Layout>
-      <Hero background={""} icon={null} section="case" heading="COAD" />
-      <Description description="Fra et simpelt mockup til en skrædersyet landingsside. Organic Masterclass er et glimrende eksempel på, hvor stor betydning en gennemarbejdet hjemmeside har for det samlede antal af konverterende besøgende." />
+      <Hero             description={<div className="w-[92%] mx-auto max-w-main flex items-center justify-end">
+            <p className="text-[22px] w-[68%] text-gray-600 font-[400] lg:text-[24px] lg:leading-[34px] lg:w-[90%] mid:w-full mid:text-[22px] mid:leading-[32px]">
+            Gazella Team udgør et hold af ambitiøse, nytænkende og storsmilende programmøre og designere, som hver dag brænder for at skabe unikke digitale produkter. Vi fokuserer på, at tilpasse hver enkel løsning til det pågældende projekt. Derfor kan du hos os forvente en fast kompetent sparingspartner, som supplerer med viden og leverer til tiden til den aftalte pris.
+            </p>
+          </div>} background={""} icon={null} section="case" heading="COAD" />
       <div className="w-[92%] mb-[200px] mx-auto max-w-main flex flex-col items-end justify-center gap-[20px] mt-[-100px]">
           <p className="text-[44px] w-[68%] text-main font-[500] lg:text-[24px] lg:leading-[34px] lg:w-[90%] mid:w-full mid:text-[22px] mid:leading-[32px]">
             +10
