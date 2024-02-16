722dc7ee48683bde6dbe54b30c2401571d6c99da
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-02-16 18:51:25 +0100

    jg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,28 +18,10 @@ import LandigServices from "@/components/landingservices";
 export default function Index() {
   return (
     <>
-      <Meta />
-      <Layout>
-
-      <section className="py-[100px] bg-[url(/heropatternsvg.svg)] bg-cover pb-[300px] w-[100%] mx-auto max-w-none hro:mt-0 hro:bg-[#f1faff] hro:py-[100px] hro:mb-[100px]">
-      <Reveal>
-        <div className="w-[92%] mx-auto max-w-main">
-          <div className="flex flex-col justify-center gap-[20px] mb-[50px]">
-          
-            <h1 className="text-[80px] font-[500] text-main mb-[8px] w-[76%] leading-[80px] lg:text-[60px] lg:leading-[70px] hro:w-full">
-              A software agency wih passion for profitable solutions
-            </h1>
-            <p className="text-[22px] text-gray-600 font-[400] mb-[30px] w-[40%] hro:w-full">We specialise in building unique, useful and profitable software for businesses and projects.</p>
-            <div className="flex items-center gap-[14px]">
-              <Link className="inline-block cursor-pointer font-[500] select-none px-6 py-2 items-center gap-[6px] text-center transition-all border-[2px] border-main rounded-full text-white bg-main" href={"/work/cases"}>See our work</Link>
-              <Link className="flex cursor-pointer font-[500] select-none px-6 py-2 items-center gap-[6px] text-center transition-all border-[2px] border-main rounded-full text-main" href={"/contact"}>Contact us<ChevronRight size={22}/></Link>
-            </div>
-          </div>
-        </div>
-      </Reveal>
-    </section>
-    <LandigServices />
-      </Layout>
+    <div className="bg-main text-center h-[100vh] text-white flex items-center flex-col justify-center">
+      <p className="text-[60px] font-[600]">Coming soon</p>
+      <p>Powered by Gazella Team</p>
+    </div>
     </>
   );
 }
